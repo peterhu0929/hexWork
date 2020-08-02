328bc24021dbec2d757a8ccece5f28c31f0b1239
peterhu0929 <peterhu0929@gmail.com>
2020-08-02 17:08:10 +0800

    adjust login.css & adjust pagination

--- week4/pagination.js
+++ week4/pagination.js
@@ -1,9 +1,9 @@
 export default {
     template: `<nav aria-label="Page navigation example">
             <ul class="pagination">
-                <li class="page-item"><a class="page-link"  href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link"  @click.prevent="changePage(pages.current_page-1)"href="#">Previous</a></li>
                 <li class="page-item" v-for="(i,key) in pages.total_pages" :key="i" :class="{active:pages.current_page===i}"@click.prevent="changePage(i)"><a class="page-link" href="#">{{i}}</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                <li class="page-item"><a class="page-link" @click.prevent="changePage(pages.current_page+1)" href="#">Next</a></li>
             </ul>
         </nav>`,
     props: ["pages"],
